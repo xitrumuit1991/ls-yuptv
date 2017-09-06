afb340cae203109cb935236bcf11b350f6c15e6e
nguyentvk <tranvokhoinguyen@gmail.com>
2017-09-06 19:09:28 +0700

    done payment v2

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-var configs = {
-  port: 5002,
-  redis_host: "localhost",
-  redis_port: 6379,
-  redis_session_secret: "1BCB58514FCE3C7C66BB6C91ACD88",
-  serve_static: true,
-
-  web_domain: "local.livestar.vn",
-  wap_domain: "m-local.livestar.vn",
-
-  live_path: "http://localhost:5001/",
-  api_domain: "http://localhost:3000",
-
-  // api_base_url: "http://localhost:3000/api/v1/",
-  api_base_url: "http://api.livestar.vn/api/v1/",
-
-  api_secrect_key: "fQ+KY11&l624Bu5",
-  web_SITE_KEY: "6LdVAxkTAAAAAAeES9kxnsXGbKuW3dcrtM2u_NH_",
-  web_SECRET_KEY: "6LdVAxkTAAAAAHBO45vG52ZMogoakzo4wopvuLke"
-};
+//set config redis here
+var configs = require('./config_server');
 
 var express = require('express');
 var path = require('path');
---
+++ config_server.js
@@ -0,0 +1,20 @@
+module.exports = {
+  port: 5002,
+  redis_host: "localhost",
+  redis_port: 6379,
+  redis_session_secret: "1BCB58514FCE3C7C66BB6C91ACD88",
+  serve_static: true,
+
+  web_domain: "local.livestar.vn",
+  wap_domain: "m-local.livestar.vn",
+
+  live_path: "http://localhost:5001/",
+  api_domain: "http://localhost:3000",
+
+  // api_base_url: "http://localhost:3000/api/v1/",
+  api_base_url: "http://api.livestar.vn/api/v1/",
+
+  api_secrect_key: "fQ+KY11&l624Bu5",
+  web_SITE_KEY: "6LdVAxkTAAAAAAeES9kxnsXGbKuW3dcrtM2u_NH_",
+  web_SECRET_KEY: "6LdVAxkTAAAAAHBO45vG52ZMogoakzo4wopvuLke"
+};
