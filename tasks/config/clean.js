da184b014c42939f01ad1bac60d86e4b0c985603
nguyentvk <tranvokhoinguyen@gmail.com>
2017-09-15 09:52:39 +0700

    done slide

--- tasks/config/clean.js
+++ tasks/config/clean.js
@@ -1,8 +1,9 @@
 module.exports = function (grunt) {
-  grunt.config.set('clean', {
-    public: ['www','www'],
-    orig :['**/*.orig']
-  });
-
+  grunt.config.set('clean',
+    {
+      public: ['www','www'],
+      orig :['**/*.orig']
+    }
+  );
   grunt.loadNpmTasks('grunt-contrib-clean');
 };
